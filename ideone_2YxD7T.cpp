a44a8a0c0f6a945b4c81ac1ca24a8e080c7c8e96
zippsy <31206072+zippsy@users.noreply.github.com>
2017-09-04 16:34:30 +0530

    Add files via upload

---
+++ ideone_2YxD7T.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int num1,num2;
+	cout<< "Enter 1st number\t";
+	cin>> num1;
+	cout<< "Enter 2nd number\t";
+	cin>> num2;
+	if(num1>num2)
+	cout<<num1<< "\t is gretest number";
+	else
+	cout<<num2<< "\t is greatest number";
+	return 0;
+}
